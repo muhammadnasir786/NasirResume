dd56affbc5f22babdf0845e482ae382e343c4bc6
Muhammad Nasir <nasir03082409229@gmail.com>
2017-08-26 10:21:04 -0700

    Add files via upload

---
+++ resume/public/main.js
@@ -0,0 +1,25 @@
+function Resume(name, designation, address, email, number, html, cssVal, javascript, jquery, bootstrap, urdu, english, firstDesignation, firstDesignationData, secondDesignation, secondDesignationData, thirdDesignation, thirdDesignationData, gradution, inter, matric) {
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("designation").innerHTML = designation;
+    document.getElementById("address").innerHTML = address;
+    document.getElementById("email").innerHTML = email;
+    document.getElementById("number").innerHTML = number;
+    document.getElementById("html").innerHTML = html;
+    document.getElementById("cssVal").innerHTML = cssVal;
+    document.getElementById("javascript").innerHTML = javascript;
+    document.getElementById("jquery").innerHTML = jquery;
+    document.getElementById("bootstrap").innerHTML = bootstrap;
+    document.getElementById("urdu").innerHTML = urdu;
+    document.getElementById("english").innerHTML = english;
+    document.getElementById("firstDesignation").innerHTML = firstDesignation;
+    document.getElementById("firstDesignationData").innerHTML = firstDesignationData;
+    document.getElementById("secondDesignation").innerHTML = secondDesignation;
+    document.getElementById("secondDesignationData").innerHTML = secondDesignationData;
+    document.getElementById("thirdDesignation").innerHTML = thirdDesignation;
+    document.getElementById("thirdDesignationData").innerHTML = thirdDesignationData;
+    document.getElementById("gradution").innerHTML = gradution;
+    document.getElementById("inter").innerHTML = inter;
+    document.getElementById("matric").innerHTML = matric;
+}
+
+Resume("Muhammad Nasir","Mobile App Developer","karachi , Pakistan","nasir03002409229@gmail.com","03082409229","80%","90%","80%","50%","60%","99%","50%","Front End Developer","Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur quisquam consectetur fugiat hic fuga corporis porro, nemo quae molestias amet. Esse accusamus rerum, mollitia! Minima doloremque aliquid, sapiente ducimus molestiae.","Web Application Developer","Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur quisquam consectetur fugiat hic fuga corporis porro, nemo quae molestias amet. Esse accusamus rerum, mollitia! Minima doloremque aliquid, sapiente ducimus molestiae.","Back End Developer","Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur quisquam consectetur fugiat hic fuga corporis porro, nemo quae molestias amet. Esse accusamus rerum, mollitia! Minima doloremque aliquid, sapiente ducimus molestiae.","BS Computer Science continue from Federal Urdu University , Karachi","Inter Pre-Engineering completed from Buffer Zone Boys Degree College , Karachi","Matric Computer Science completed from Mehran technical High School , Karachi")
